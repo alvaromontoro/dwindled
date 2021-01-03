39775aa287bbdeb45a9729b73c22bb4dc7af2cda
Alvaro Montoro <alvaromontoro@gmail.com>
2021-01-03 09:43:05 -0600

    Refactor code.js
    Convert the code into an object where all the elements and functions are encapsulated instead of having everything at the global scope.

--- js/code.js
+++ js/code.js
@@ -1,27 +1,36 @@
-const button = document.querySelector("#dwindle-button");
-const fromBox = document.querySelector("#text");
-const toBox = document.querySelector("#result");
-const fromCount = document.querySelector("#text-count");
-const toCount = document.querySelector("#result-count");
+const dwindle = {
+  // the DOM elements needed   
+  elements: {
+    button: document.querySelector("#dwindle-button"),
+    fromBox: document.querySelector("#text"),
+    toBox: document.querySelector("#result"),
+    fromCount: document.querySelector("#text-count"),
+    toCount: document.querySelector("#result-count")
+  },
+  // the reduction/dwindling function
+  dwindle: function (text) {
+    return text.replace(/ one /ig, " <span class='key' data-original='one'>1</span> ");
+  },
+  // component initialization: add events, load data, etc.
+  init: function () {
+    dwindle.elements.button.addEventListener("click", function (e) {
+      const originalText = dwindle.elements.fromBox.value.trim();
+      if (originalText !== "") {
+        const transformedText = dwindle.dwindle(originalText);
+        dwindle.elements.toBox.innerHTML = transformedText;
+        dwindle.elements.toBox.classList.add("processed");
+        dwindle.elements.toCount.textContent = dwindle.elements.toBox.textContent.length;
+      } else {
+        dwindle.elements.toBox.innerHTML = "";
+        dwindle.elements.toBox.classList.remove("processed");
+        dwindle.elements.toCount.textContent = "0";
+      }
+    });
 
-function dwindle(text) {
-  return text.replace(/ one /ig, " <span class='processed'>1</span> ");
+    dwindle.elements.fromBox.addEventListener("input", function () {
+      dwindle.elements.fromCount.textContent = dwindle.elements.fromBox.value.length;
+    })
+  }
 }
 
-button.addEventListener("click", function (e) {
-  const originalText = fromBox.value.trim();
-  if (originalText !== "") {
-    const transformedText = dwindle(originalText);
-    toBox.innerHTML = transformedText;
-    toBox.classList.add("processed");
-    toCount.textContent = toBox.textContent.length;
-  } else {
-    toBox.innerHTML = "";
-    toBox.classList.remove("processed");
-    toCount.textContent = "0";
-  }
-});
-
-fromBox.addEventListener("input", function () {
-  fromCount.textContent = fromBox.value.length;
-})
+dwindle.init();
