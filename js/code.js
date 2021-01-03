58e3d5f848396708996c5b700cab01631e38619b
Alvaro Montoro <alvaromontoro@gmail.com>
2021-01-02 22:15:56 -0600

    Initial styles and functionality
    - Replace the word "One" with "1"
    - Add intiial responsive styles
    - Adjust HTML

--- js/code.js
+++ js/code.js
@@ -1,13 +1,27 @@
 const button = document.querySelector("#dwindle-button");
 const fromBox = document.querySelector("#text");
 const toBox = document.querySelector("#result");
+const fromCount = document.querySelector("#text-count");
+const toCount = document.querySelector("#result-count");
 
 function dwindle(text) {
-  return text.replace(/ one /ig, " 1 ");
+  return text.replace(/ one /ig, " <span class='processed'>1</span> ");
 }
 
 button.addEventListener("click", function (e) {
-  const originalText = fromBox.value;
-  const transformedText = dwindle(originalText);
-  toBox.value = transformedText;
+  const originalText = fromBox.value.trim();
+  if (originalText !== "") {
+    const transformedText = dwindle(originalText);
+    toBox.innerHTML = transformedText;
+    toBox.classList.add("processed");
+    toCount.textContent = toBox.textContent.length;
+  } else {
+    toBox.innerHTML = "";
+    toBox.classList.remove("processed");
+    toCount.textContent = "0";
+  }
 });
+
+fromBox.addEventListener("input", function () {
+  fromCount.textContent = fromBox.value.length;
+})
