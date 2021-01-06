083f9e81254de1b318fdeb15bdec7528695f8279
Alvaro Montoro <alvaromontoro@gmail.com>
2021-01-05 21:34:58 -0600

    Add switch option
    Allow users to switch between old and new values if the text is long enough. Add keyboard support for it (required big refactor :( )

--- js/code.js
+++ js/code.js
@@ -7,6 +7,8 @@ const dwindle = {
     fromCount: document.querySelector("#text-count"),
     toCount: document.querySelector("#result-count"),
     bubble: document.querySelector("#replacement-info"),
+    termOriginal: document.querySelector("#replacement-info-original"),
+    termChange: document.querySelector("#replacement-info-change"),
     screenreader: document.querySelector("#screen-reader"),
     tweetButton: document.querySelector("#tweet-button-container")
   },
@@ -14,6 +16,8 @@ const dwindle = {
   languages: ["en"],
   types: ["numbers", "ordinals", "popular", "corporations"],
   loaded: 0,
+  activeKey: null,
+  activeMenu: -1,
   data: {},
   // load ALL the data from the JSON files
   loadData: function () {
@@ -40,6 +44,16 @@ const dwindle = {
       }
     }
   },
+  updateLink: function () {
+    if (this.elements.toBox.textContent.length <= 280) {
+      this.elements.tweetButton.querySelector("a").setAttribute("href", `https://twitter.com/intent/tweet?text=${encodeURI(this.elements.toBox.textContent.trim())}&url=`);
+      this.elements.tweetButton.style.visibility = "visible";
+      this.speak("The dwindled text is ready to be tweeted");
+    } else {
+      this.elements.tweetButton.style.visibility = "hidden";
+      this.speak("The text is still too long to be tweeted")
+    }
+  },
   // actual replacement function!
   replace: function (text, language, type) {
     if (this.data && this.data[language] && this.data[language][type]) {
@@ -52,12 +66,34 @@ const dwindle = {
           const regexp = new RegExp(pattern, "gi");
           text = text.replace(regexp, function (match, group1, group2) {
             const remediate = match.substring(group1.length, match.length - group1.length - group2.length + 1);
-            return `${group1}<button class="key" tabindex="0" data-original="${remediate}" data-change="${dictionary[keys[x]]}">${dictionary[keys[x]]}</button>${group2}`;
+            return `${group1}<button class="key" 
+                                     aria-haspopup="menu"
+                                     tabindex="0"
+                                     data-original="${remediate}" 
+                                     data-change="${dictionary[keys[x]]}">${dictionary[keys[x]]}</button>${group2}`;
           });
         }
       }
     }
     return text;
+  },
+  speak: function (text) {
+    this.elements.screenreader.textContent = text;
+    setTimeout(() => { this.elements.screenreader.textContent = ""; }, 1000);
+  },
+  hideBubble: function (force = false) {
+    this.elements.bubble.style = "";
+    
+    if (this.activeKey && !force) {
+      this.activeKey.focus();
+      this.activeKey = null;
+    }
+
+    this.activeMenu = -1;
+    const menu = this.elements.bubble.querySelector(".focused");
+    if (menu) {
+      menu.classList.remove("focused");
+    }
   },
   // the reduction/dwindling function
   dwindle: function (text) {
@@ -81,12 +117,7 @@ const dwindle = {
         this.elements.toBox.classList.add("processed");
         this.elements.toCount.textContent = this.elements.toBox.textContent.length;
         this.elements.toBox.focus();
-        if (this.elements.toBox.textContent.length <= 280) {
-          this.elements.tweetButton.querySelector("a").setAttribute("href", `https://twitter.com/intent/tweet?text=${encodeURI(this.elements.toBox.textContent.trim())}&url=`);
-          this.elements.tweetButton.style.visibility = "visible";
-        } else {
-          this.elements.tweetButton.style.visibility = "hidden";
-        }
+        this.updateLink();
       } else {
         this.elements.toBox.innerHTML = "";
         this.elements.toBox.classList.remove("processed");
@@ -102,29 +133,88 @@ const dwindle = {
     this.elements.toBox.addEventListener('click', (e) => {
       if (e.target.className.toLowerCase() === 'key') {
         const key = e.target;
+        this.activeKey = key;
         const coordinates = key.getBoundingClientRect();
         this.elements.bubble.querySelector("#replacement-info-original").textContent = key.dataset.original;
         this.elements.bubble.querySelector("#replacement-info-change").textContent = key.dataset.change;
         this.elements.bubble.style = `top: ${coordinates.top + window.scrollY}px; left: ${coordinates.left}px; display: block;`;
-        this.elements.screenreader.textContent = this.elements.bubble.textContent;
-        setTimeout(() => { this.elements.screenreader.textContent = ""; }, 1000);
-      }
+        this.speak(this.elements.bubble.textContent);
 
-      e.target.onblur = () => {
-        this.elements.bubble.style = "";
-        e.target.onblur = "";
+        e.target.onblur = (event) => {
+          if (event.relatedTarget && event.relatedTarget.className.toLowerCase() !== "term") {
+            this.hideBubble(true);
+          }
+          e.target.onblur = "";
+          e.target.onkeydown = "";
+        }
+
+        e.target.onkeydown = (event) => {
+          if (this.activeKey) {
+            switch (event.key) {
+              case "Esc":
+              case "Escape":
+                this.hideBubble();
+                break;
+              case "Enter":
+              case "Space":
+                event.preventDefault();
+                if (this.activeMenu > 0) {
+                  const focused = this.elements.bubble.querySelector(`button:nth-of-type(${this.activeMenu})`);
+                  this.activeKey.textContent = focused.textContent;
+                  this.activeKey.dataset.isoriginal = this.activeMenu === 1;
+                  this.elements.toCount.textContent = this.elements.toBox.textContent.length;
+                  this.speak(`Selected ${focused.textContent} (${this.activeMenu === 1 ? "original" : "new"} value)`);
+                  this.updateLink();
+                  this.hideBubble();
+                }
+                this.hideBubble();
+                break;
+              case "Up":
+              case "ArrowUp":
+              case "Down":
+              case "ArrowDown":
+                event.preventDefault();
+                const focused = this.elements.bubble.querySelector(".focused");
+                if (focused) {
+                  focused.classList.remove("focused");
+                }
+                this.activeMenu = this.activeMenu === 1 ? 2 : 1;
+                const newFocused = this.elements.bubble.querySelector(`button:nth-of-type(${this.activeMenu})`);
+                newFocused.classList.add("focused");
+                this.speak(`${newFocused.textContent} (${this.activeMenu === 1 ? "original" : "new"} value)`);
+                break;
+            }
+          }
+        }
       }
     });
+
+    this.elements.termChange.addEventListener("click", () => {
+      this.activeKey.textContent = this.activeKey.dataset.change;
+      this.activeKey.dataset.isoriginal = false;
+      this.elements.toCount.textContent = this.elements.toBox.textContent.length;
+      this.updateLink();
+      this.hideBubble();
+    });
+
+    this.elements.termOriginal.addEventListener("click", () => {
+      this.activeKey.textContent = this.activeKey.dataset.original;
+      this.activeKey.dataset.isoriginal = true;
+      this.elements.toCount.textContent = this.elements.toBox.textContent.length;
+      this.updateLink();
+      this.hideBubble();
+    });
+
 
     window.addEventListener("click", (e) => {
       if (e.target.className.toLowerCase() !== 'key') {
-        this.elements.bubble.style = "";
+        this.hideBubble(true);
       }
     });
 
-    this.elements.toBox.addEventListener("scroll", () => { this.elements.bubble.style = ""; });
-    window.addEventListener("scroll", () => { this.elements.bubble.style = ""; });
-    window.addEventListener("resize", () => { this.elements.bubble.style = ""; });
+    this.elements.toBox.addEventListener("scroll", () => { this.hideBubble(); });
+    window.addEventListener("scroll", () => { this.hideBubble(); });
+    window.addEventListener("resize", () => { this.hideBubble(); });
   }
 }
 
