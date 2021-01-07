c1657d022dc3ed266e60cc328b5d90adbef8feea
Alvaro Montoro <alvaromontoro@gmail.com>
2021-01-07 17:25:51 -0600

    Move serviceworker

--- js/code.js
+++ js/code.js
@@ -249,7 +249,7 @@ dwindle.init();
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function () {
     navigator.serviceWorker
-      .register("./js/serviceWorker.js")
+      .register("/serviceWorker.js")
       .then(res => console.log("Service worker registered"))
       .catch(err => console.log("Service worker not registered", err));
   });
